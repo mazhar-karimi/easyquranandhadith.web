99ffdff1b674868cea9ca4aacfa43947da2f4411
mazhar-karimi <mazharkarimi@gmail.com>
2018-07-02 17:21:14 +0500

    Add files via upload

--- eqwh.web/Controllers/AyahController.cs
+++ eqwh.web/Controllers/AyahController.cs
@@ -6,30 +6,38 @@ using System.Net.Http;
 using System.Web.Http;
 using eqwh.web.Factory;
 using eqwh.web.Models;
+using System.Threading.Tasks;
+using eqwh.web.Common;
 namespace eqwh.web.Controllers
 {
     public class AyahController : ApiController
     {
         [Route("Ayah/GetAyat/{ayahno}")]
         [HttpGet]
-        
-        public AyatRow GetAyat(int ayahno)
+        [CacheFilter(TimeDuration = 1800)]
+        public async Task<AyatRow> GetAyat(int ayahno)
         {
             if (ayahno > 6348)
                 return null;
 
-                return DataReaderFactory.Get.GetAyat(ayahno);
+            AyatRow obj = null;
+            var t = Task.Run(() => obj = DataReaderFactory.Get().GetAyat(ayahno));
+            t.Wait();
+
+            return obj;
         }
 
         [Route("Ayah/SearchAyat/{word}")]
         [HttpGet]
-
-        public int[] SearchAyat(string word)
+        [CacheFilter(TimeDuration = 1800)]
+        public async Task<int[]> SearchAyat(string word)
         {
             if (String.IsNullOrEmpty(word))
                 return null;
-
-            return DataReaderFactory.Get.SearchAyat(word);
+            int[] data = null;
+            var t = Task.Run(() => data = DataReaderFactory.Get().SearchAyat(word));
+            t.Wait();
+            return data;
         }
     }
 }
---
+++ eqwh.web/Controllers/HadithController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using eqwh.web.Factory;
+using eqwh.web.Models;
+using System.Threading.Tasks;
+using eqwh.web.Common;
+
+namespace eqwh.web.Controllers
+{
+    public class HadithController : ApiController
+    {
+
+        [Route("Hadith/GetHadith/{hadithno}")]
+        [HttpGet]
+        [CacheFilter(TimeDuration = 1800)]
+        public async Task<Ahadith> GetHadith(int hadithno)
+        {
+            Ahadith obj = null;
+            var t = Task.Run(() => obj = DataReaderFactory.Get(true).GetHadith(hadithno));
+            t.Wait();
+
+            return obj;
+        }
+
+    }
+}
